2c712988e03a17da9c7b31f65d5a0206763d1938
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-08-20 22:06:09 +0530

    Time: 7 ms (46.71%), Space: 42 MB (84.12%) - LeetHub

---
+++ 1216-print-zero-even-odd/1216-print-zero-even-odd.java
@@ -0,0 +1,46 @@
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
+class ZeroEvenOdd {
+    private int n;
+    private Semaphore zeroSem = new Semaphore(1);
+    private Semaphore evenSem = new Semaphore(0);
+    private Semaphore oddSem = new Semaphore(0);
+    private int current = 1;
+    
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            zeroSem.acquire();
+            printNumber.accept(0);
+            
+            if (current % 2 == 1) {
+                oddSem.release(); // Release odd semaphore for odd number
+            } else {
+                evenSem.release(); // Release even semaphore for even number
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            evenSem.acquire();
+            printNumber.accept(current);
+            current++;
+            zeroSem.release(); // Release zero semaphore for next zero
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            oddSem.acquire();
+            printNumber.accept(current);
+            current++;
+            zeroSem.release(); // Release zero semaphore for next zero
+        }
+    }
+}
