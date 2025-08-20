cea442cf4ded15845e8cb795dd0cb929112880e4
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-08-20 22:03:35 +0530

    Time: 1 ms (72.1%), Space: 45.9 MB (29.04%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,19 +2,19 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         boolean firstrow = false;
         boolean firstcol = false;
-        for(int i=0;i<matrix.length;i++){
+        for(int i = 0 ;i<matrix.length;i++){
             for(int j = 0 ;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
                     if(i==0) firstrow = true;
-                    if(j==0) firstcol =true;
+                    if(j==0) firstcol = true;
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
         for(int i = 1;i<matrix.length;i++){
-            for(int j = 1;j<matrix[0].length;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j] = 0;
+            for(int j =1;j<matrix[0].length;j++){
+                if(matrix[i][0]==0 ||matrix[0][j]==0) matrix[i][j] = 0;
             }
         }
         if(firstrow){
@@ -23,11 +23,10 @@ class Solution {
             }
         }
         if(firstcol){
-            for(int i = 0;i<matrix.length;i++){
+            for(int i = 0 ;i<matrix.length;i++){
                 matrix[i][0] = 0;
             }
         }
-        
 
 
     }
