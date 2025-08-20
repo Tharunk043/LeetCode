af1807f32a71b9525743d61072cbd7fd7b1bce5f
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-08-20 20:53:45 +0530

    Time: 0 ms (100%), Space: 40.7 MB (25.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,18 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        if(n==1 && m==1) return 1;
         int[][] dp = new int[m][n];
-        
-        // first row and first column can only be reached one way
-        for (int i = 0; i < m; i++) dp[i][0] = 1;
-        for (int j = 0; j < n; j++) dp[0][j] = 1;
-        
-        // fill the rest
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        for(int j =1;j<n;j++){
+            dp[0][j] = 1;
+        }
+        for(int i =1;i<m;i++){
+            dp[i][0] = 1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
             }
         }
-        
         return dp[m-1][n-1];
     }
 }
