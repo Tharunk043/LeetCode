8bea897dd1cff0d162765f2d812a04f697559340
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-08-20 21:41:39 +0530

    Time: 7 ms (98.46%), Space: 48.1 MB (12.99%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        // Initialize DP table with large values
+        int[][] dp = new int[m][n];
+        
+        // First pass: top-left to bottom-right
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = Integer.MAX_VALUE - 1; // Avoid overflow
+                    
+                    // Check top neighbor
+                    if (i > 0) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                    }
+                    
+                    // Check left neighbor
+                    if (j > 0) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                    }
+                }
+            }
+        }
+        
+        // Second pass: bottom-right to top-left
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (mat[i][j] != 0) {
+                    // Check bottom neighbor
+                    if (i < m - 1) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1);
+                    }
+                    
+                    // Check right neighbor
+                    if (j < n - 1) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1);
+                    }
+                }
+            }
+        }
+        
+        return dp;
+    }
+}
