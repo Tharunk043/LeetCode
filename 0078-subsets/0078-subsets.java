97edca67924955a8b50c281f60951e7ef8cf9398
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-08-20 12:56:14 +0530

    Time: 1 ms (49%), Space: 43.1 MB (10.12%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -4,14 +4,13 @@ class Solution {
         backtrack(0,new ArrayList<>(),nums,res);
         return res;
     }
-    void backtrack(int start,List<Integer> tempset,int[] nums,List<List<Integer>> res){
-        res.add(new ArrayList<Integer>(tempset));
-        for(int i = start;i<nums.length;i++){
+    public void backtrack(int start,List<Integer> tempset,int[] nums,List<List<Integer>> res){
+        res.add(new ArrayList<>(tempset));
+        for(int i = start; i<nums.length;i++){
             tempset.add(nums[i]);
             backtrack(i+1,tempset,nums,res);
             tempset.remove(tempset.size()-1);
         }
     }
-
 
 }
