eeea12c33880087bbae8c0eb660fcde5eef2c16b
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-08-23 13:53:53 +0530

    Time: 0 ms (100%), Space: 41.7 MB (50.89%) - LeetHub

---
+++ 1040-maximum-binary-tree-ii/1040-maximum-binary-tree-ii.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+
+        // Case 1: val > root.val → val becomes new root
+        if (val > root.val) {
+            TreeNode newRoot = new TreeNode(val);
+            newRoot.left = root;
+            return newRoot;
+        }
+
+        // Case 2: insert into right subtree
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
+    }
+}
