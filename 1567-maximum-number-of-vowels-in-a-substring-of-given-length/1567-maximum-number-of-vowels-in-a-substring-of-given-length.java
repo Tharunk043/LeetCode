c565f7fb1b0ce8fbd9762d914fc0893bcf05ad6d
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-08-25 20:23:01 +0530

    Time: 22 ms (35.51%), Space: 45.1 MB (19.04%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,23 +1,22 @@
 class Solution {
     public int maxVowels(String s, int k) {
+        int maxv = 0;
+        int windv =0;
         HashSet<Character> vowels = new HashSet<>();
-        int windowv =0;
-        int maxv =0;
         vowels.add('a');
-        vowels.add('e');
         vowels.add('i');
         vowels.add('o');
+        vowels.add('e');
         vowels.add('u');
-        for(int i = 0;i<k;i++){
-            if(vowels.contains(s.charAt(i))) windowv++;
+        for(int i = 0 ;i<k;i++){
+            if(vowels.contains(s.charAt(i))) windv ++;
         }
-        maxv = windowv;
+        maxv= windv;
         for(int i = k;i<s.length();i++){
-            if(vowels.contains(s.charAt(i-k)) ) windowv--;
-            if(vowels.contains(s.charAt(i))) windowv++;
-            maxv = Math.max(windowv,maxv);
-            if(maxv==k) break;
-        }
+            if(vowels.contains(s.charAt(i-k))) windv--;
+            if(vowels.contains(s.charAt(i))) windv++;
+            maxv = Math.max(maxv,windv);
+        } 
         return maxv;
 
     }
