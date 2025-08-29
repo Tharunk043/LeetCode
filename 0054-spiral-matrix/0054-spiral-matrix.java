57e861160da5a452b816fb1a80de55c2d4f65b50
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-08-29 15:23:24 +0530

    Time: 0 ms (100%), Space: 41.9 MB (15.68%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,29 +1,25 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new ArrayList<>();
-        int rowbeg =0;
+        int rowbeg = 0;
+        int colbeg =0;
         int rowend = matrix.length-1;
-        int colbeg = 0;
         int colend = matrix[0].length-1;
         while(rowbeg<=rowend && colbeg<=colend){
-            //to traverse left to right
             for(int j = colbeg;j<=colend;j++){
                 res.add(matrix[rowbeg][j]);
             }
-            //to traverse up to down
             rowbeg++;
             for(int j = rowbeg;j<=rowend;j++){
                 res.add(matrix[j][colend]);
             }
             colend--;
-            //to traverse right to left
             if(rowbeg<=rowend){
                 for(int j = colend;j>=colbeg;j--){
                     res.add(matrix[rowend][j]);
                 }
             }
             rowend--;
-            //to travers from down to up
             if(colbeg<=colend){
                 for(int j = rowend;j>=rowbeg;j--){
                     res.add(matrix[j][colbeg]);
