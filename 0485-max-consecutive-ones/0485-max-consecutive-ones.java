c4c3b94e50040f55ef1502959c4f11a79165798f
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-08-29 20:54:52 +0530

    Time: 2 ms (95.06%), Space: 49.9 MB (96.12%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -5,11 +5,13 @@ class Solution {
         for(int i = 0;i<nums.length;i++){
             if(nums[i]==1){
                 count++;
-                max=Math.max(count,max);
             }else{
+                max = Math.max(max,count);
                 count=0;
+
             }
         }
+         max = Math.max(max,count);
         return max;
     }
 }
