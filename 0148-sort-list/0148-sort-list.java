8ab00c42010c974185c47efc9effc333a29bd191
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-08-30 22:09:18 +0530

    Time: 9 ms (96.11%), Space: 56.8 MB (61.04%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -11,33 +11,32 @@
 class Solution {
     public ListNode sortList(ListNode head) {
         if(head==null || head.next==null) return head;
-        //first get mid
-        ListNode slow=head;
+        ListNode mid = head;
         ListNode fast = head.next;
         while(fast!=null && fast.next!=null){
-            slow = slow.next;
+            mid = mid.next;
             fast = fast.next.next;
         }
-        ListNode right = slow.next;
-        slow.next = null;
+        ListNode right = mid.next;
+        mid.next = null;// to break the list
         ListNode l1 = sortList(head);
         ListNode l2 = sortList(right);
         return merge(l1,l2);
     }
     ListNode merge(ListNode l1,ListNode l2){
-        ListNode dummy = new ListNode();
+        ListNode dummy= new ListNode();
         ListNode curr = dummy;
         while(l1!=null && l2!=null){
             if(l1.val<=l2.val){
-                curr.next = l1;
+                curr.next =l1;
                 l1 = l1.next;
             }else{
                 curr.next = l2;
                 l2 = l2.next;
             }
             curr = curr.next;
-        }   
-        curr.next = (l1!=null)?l1:l2;
+        }
+        curr.next = l1!=null?l1:l2;
         return dummy.next;
     }
 }
