6a0686a86e0cd5fd9dc153937f0ae675cdbafb8d
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-09-02 19:52:22 +0530

    Time: 1 ms (79.56%), Space: 41.8 MB (45.9%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        char[] ch = new char[26];
+        int[] ch = new int[26];
         for(char c:sentence.toCharArray()){
             ch[c-'a'] = 1;
         }
-        for(char c:ch){
+        for(int c:ch){
             if(c!=1) return false;
         }
         return true;
