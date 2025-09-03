63090b0deaaf42b1083c8dd92768e4343e2af56c
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-09-03 14:57:52 +0530

    Time: 41 ms (61.14%), Space: 44.2 MB (68.21%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -4,9 +4,7 @@ class Solution {
         for(int i =1 ;i<nums.length;i++){
             for(int j = 0;j<i;j++){
                 if(nums[i]>nums[j]){
-                    if(dp[j]+1>dp[i]){
-                        dp[i] = dp[j]+1;
-                    }
+                        dp[i] =Math.max(dp[i], dp[j]+1);
                 }
             }
         }
