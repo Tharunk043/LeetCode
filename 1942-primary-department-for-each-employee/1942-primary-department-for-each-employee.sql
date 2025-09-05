0530ad40427f62fa126c4f7193e484ec08d0a08b
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-09-05 13:06:56 +0530

    Time: 550 ms (52.29%), Space: 0B (100%) - LeetHub

---
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+
+UNION
+
+SELECT employee_id, MIN(department_id) AS department_id
+FROM Employee
+WHERE employee_id NOT IN (
+    SELECT employee_id
+    FROM Employee
+    WHERE primary_flag = 'Y'
+)
+GROUP BY employee_id;
+
