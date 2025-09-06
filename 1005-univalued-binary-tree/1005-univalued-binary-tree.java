7034733029fa8cefcb96b72c1cf2b201fa03466d
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-09-06 10:49:06 +0530

    Time: 0 ms (100%), Space: 40.9 MB (95.24%) - LeetHub

--- 1005-univalued-binary-tree/1005-univalued-binary-tree.java
+++ 1005-univalued-binary-tree/1005-univalued-binary-tree.java
@@ -1,27 +1,12 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isUnivalTree(TreeNode root) {
         if (root == null) return true;
-        return dfs(root, root.val);
+        return helper(root, root.val);
     }
 
-    private boolean dfs(TreeNode node, int val) {
+    private boolean helper(TreeNode node, int val) {
         if (node == null) return true;
         if (node.val != val) return false;
-        return dfs(node.left, val) && dfs(node.right, val);
+        return helper(node.left, val) && helper(node.right, val);
     }
 }
