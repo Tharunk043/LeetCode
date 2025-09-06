e479ab9e4f102e96b882787da413d3597d0374e0
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-09-06 14:44:19 +0530

    Time: 2 ms (98%), Space: 45.9 MB (94.58%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,23 +1,29 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        if(mat.length==0) return new int[0];
-        int m= mat.length,n = mat[0].length;
-        int [] res =new int[m*n];
-        int r = 0;
-        int c =0 ;
-        for(int i = 0;i<res.length;i++){
-            res[i] =mat[r][c];
+        if(mat.length==0) return new int[]{};
+        int m = mat.length;
+        int n = mat[0].length;
+        int r= 0;
+        int c= 0;
+        int res[] = new int [m*n];
+        int ind =0;
+        while(ind<res.length){
+            res[ind++] = mat[r][c];
             if((r+c)%2==0){
-                if(c==n-1) r++;
-                else if(r==0) c++;
-                else{
+                if(c==n-1){
+                    r++;
+                }else if(r==0){
+                    c++;
+                }else{
                     r--;
                     c++;
                 }
             }else{
-                if(r==m-1) c++;
-                else if(c==0) r++;
-                else{
+                if(r==m-1){
+                    c++;
+                }else if(c==0){
+                    r++;
+                }else{
                     r++;
                     c--;
                 }
