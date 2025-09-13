cbb6b07fd4f885b70557dc874a838972f9dffa50
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-09-13 13:30:49 +0530

    Time: 1 ms (100%), Space: 42.6 MB (64.55%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -1,24 +1,18 @@
 class Solution {
     public int maxFreqSum(String s) {
-        int[] vowelFreq = new int[26];
-        int[] consFreq  = new int[26];
-
-        for (char ch : s.toCharArray()) {
-            if (!Character.isLetter(ch)) continue;
-            char c = Character.toLowerCase(ch);
-            if (isVowel(c)) vowelFreq[c - 'a']++;
-            else            consFreq[c - 'a']++;
+        int [] freq= new int[26];
+        for(char c:s.toCharArray()){
+            freq[c-'a']++;
         }
-
-        int maxV = 0, maxC = 0;
-        for (int i = 0; i < 26; i++) {
-            if (vowelFreq[i] > maxV) maxV = vowelFreq[i];
-            if (consFreq[i]  > maxC) maxC = consFreq[i];
+        int vmax = 0;
+        int cmax = 0;
+        for(char c:s.toCharArray()){
+            if("aeiou".indexOf(c)!=-1){
+                vmax = Math.max(vmax,freq[c-'a']);
+            }else{
+                cmax = Math.max(cmax,freq[c-'a']);
+            }
         }
-        return maxV + maxC;
-    }
-
-    private boolean isVowel(char c) {
-        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+        return vmax+cmax;
     }
 }
