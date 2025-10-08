d0b04bc2f7c9ab7e92544508b857d20d8a35a7f1
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-10-08 10:27:59 +0530

    Time: 2 ms (30.49%), Space: 44.7 MB (90.58%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,8 +1,7 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int[][] res = new int[intervals.length+1][2];
-        int i = 0;
-        int j = 0;
+        int i=0,j = 0;
         while(i<intervals.length && intervals[i][1]<newInterval[0]){
             res[j++] = intervals[i++];
         }
@@ -10,10 +9,10 @@ class Solution {
             newInterval[0] = Math.min(newInterval[0],intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1],intervals[i++][1]);
         }
-        res[j++] =newInterval;
+        res[j++] = newInterval;
         while(i<intervals.length){
             res[j++]= intervals[i++];
-        } 
+        }
         return Arrays.copyOf(res,j);
     }
 }
