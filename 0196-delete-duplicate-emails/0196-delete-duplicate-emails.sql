03e7eaec1c9c504b472d86b9de935c743dd5009d
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-10-17 16:02:06 +0530

    Time: 614 ms (84.6%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
-delete from person
+delete from Person
 where id not in (
     select id from (
-    select id ,row_number() over (partition by email order by id) as rn from person
+        select id, row_number() over (partition by email order by id) as rn from Person
     ) as t
     where rn = 1
 )
