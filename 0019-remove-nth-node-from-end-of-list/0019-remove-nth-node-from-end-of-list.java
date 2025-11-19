16403b0174148209ca40c4162eb9512ea0a352af
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2025-11-19 09:47:13 +0530

    Time: 0 ms (100%), Space: 43.3 MB (23%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -14,12 +14,12 @@ class Solution {
         dummy.next = head;
         ListNode p1 = dummy;
         ListNode p2 = dummy;
-        for(int i =0;i<n;i++){
+        for(int i = 0;i<n;i++){
             p2 = p2.next;
         }
         while(p2.next!=null){
             p1 = p1.next;
-            p2 =p2.next;
+            p2 = p2.next;
         }
         p1.next = p1.next.next;
         return dummy.next;
