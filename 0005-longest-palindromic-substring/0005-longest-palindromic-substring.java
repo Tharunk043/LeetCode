e597e562c0d0fee0243117701109882271950561
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2026-01-04 14:19:58 +0530

    Time: 19 ms (44.29%), Space: 46.7 MB (38.05%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,13 +1,13 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String longest = "";
-        for(int i = 0;i<s.length();i++){
-            String even = expand(s,i,i);
-            String odd = expand(s,i,i+1);
-            String curr = even.length()>odd.length()?even:odd;
-            longest = curr.length()>longest.length()?curr:longest;
-        }
-        return longest;
+       String longest = "";
+       for(int i = 0;i<s.length();i++){
+        String even = expand(s,i,i);
+       String odd = expand(s,i,i+1);
+        String curr = even.length()>odd.length()?even:odd;
+       longest = curr.length()>longest.length()?curr:longest;
+       } 
+       return longest;
     }
     String expand(String s,int left,int right){
         while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
