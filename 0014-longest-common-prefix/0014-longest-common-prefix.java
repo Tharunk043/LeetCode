37d098d1bb5d4bc581bd6603db8f2f08d5c0b4df
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2026-01-17 09:37:41 +0530

    Time: 1 ms (64.88%), Space: 43.1 MB (73.13%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,9 +3,7 @@ class Solution {
         for(int i = 0;i<strs[0].length();i++){
             char ch = strs[0].charAt(i);
             for(int j = 1;j<strs.length;j++){
-                if(i==strs[j].length() || ch!=strs[j].charAt(i)){
-                    return strs[0].substring(0,i);
-                }
+                if(i==strs[j].length() || ch!=strs[j].charAt(i)) return strs[0].substring(0,i);
             }
         }
         return strs[0];
