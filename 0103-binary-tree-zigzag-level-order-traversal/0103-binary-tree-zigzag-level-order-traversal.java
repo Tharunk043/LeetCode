b8f19556367c98e537902458bafb160ab9f9750f
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2026-01-30 15:19:57 +0530

    Time: 1 ms (81.57%), Space: 44.1 MB (5.72%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,37 +15,30 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        boolean ltr = true;
         List<List<Integer>> res = new ArrayList<>();
         if(root==null) return res;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        int flag = 1;
         while(!queue.isEmpty()){
-            List<Integer> level = new ArrayList<>();
             int ls = queue.size();
-            if(flag==1){
+            List<Integer> list =new ArrayList<>();
+            Stack<Integer> stack = new Stack<>();
             for(int i = 0;i<ls;i++){
                 TreeNode curr = queue.poll();
-                level.add(curr.val);
+                if(ltr){
+                    list.add(curr.val);
+                }else{
+                    stack.push(curr.val);
+                }
                 if(curr.left!=null) queue.offer(curr.left);
                 if(curr.right!=null) queue.offer(curr.right);
             }
-            res.add(level);
-            flag=0;
-            }else{
-                Stack<Integer> stack = new Stack<>();
-                for(int i = 0;i<ls;i++){
-                    TreeNode curr = queue.poll();
-                    stack.push(curr.val);
-                    if(curr.left!=null) queue.offer(curr.left);
-                    if(curr.right!=null) queue.offer(curr.right);
-                }
-                while(!stack.isEmpty()){
-                    level.add(stack.pop());
-                }
-                res.add(level);
-                flag=1;
+            while(!stack.isEmpty()){
+                list.add(stack.pop());
             }
+            res.add(list);
+            ltr = !ltr;
         }
         return res;
     }
