623d0e4b946197c59ae35bf2987b7bcc2193d533
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2026-01-30 14:57:46 +0530

    Time: 1 ms (59.32%), Space: 47.6 MB (24.81%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,26 +2,26 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         boolean firstrow = false;
         boolean firstcol = false;
-        for(int i = 0;i<matrix.length;i++){
+        for(int i = 0; i <matrix.length;i++){
             for(int j = 0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                    if(i==0) firstrow=true;
+                    if(i==0) firstrow = true;
                     if(j==0) firstcol = true;
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
                 }
             }
         }
-        for(int i =1;i<matrix.length;i++){
+        for(int i = 1;i<matrix.length;i++){
             for(int j = 1;j<matrix[0].length;j++){
-                if(matrix[0][j]==0 ||matrix[i][0]==0){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
                     matrix[i][j] = 0;
                 }
             }
         }
         if(firstrow){
-            for(int j = 0;j<matrix[0].length;j++){
-                matrix[0][j]=0;
+            for(int j = 0 ;j<matrix[0].length;j++){
+                matrix[0][j] = 0;
             }
         }
         if(firstcol){
