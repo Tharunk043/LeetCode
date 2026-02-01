cffb812523959d77a596069600870a8de460935b
Velamakuru Tharun Kumar <135613245+Tharunk043@users.noreply.github.com>
2026-02-01 09:47:01 +0530

    Time: 0 ms (100%), Space: 42.7 MB (71.77%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,38 +1,44 @@
-import java.util.Stack;
-
 class MyQueue {
-    private Stack<Integer> inStack;
-    private Stack<Integer> outStack;
-
-    public MyQueue() {
-        inStack = new Stack<>();
-        outStack = new Stack<>();
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+     public MyQueue() {
+        
     }
     
     public void push(int x) {
-        inStack.push(x);
+        stack1.push(x);
     }
     
     public int pop() {
-        shiftStacks();
-        return outStack.pop();
+        if(empty()) return -1;
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
     }
     
     public int peek() {
-        shiftStacks();
-        return outStack.peek();
+        if(empty()) return -1;
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
     }
     
     public boolean empty() {
-        return inStack.isEmpty() && outStack.isEmpty();
-    }
-    
-    // Helper: move elements only when needed
-    private void shiftStacks() {
-        if (outStack.isEmpty()) {
-            while (!inStack.isEmpty()) {
-                outStack.push(inStack.pop());
-            }
-        }
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
